f414259494707d566b02a480617b37af570c128e
Tom Anderson <tom.h.anderson@gmail.com>
2012-03-09 11:49:06 -0800

    Fixed for new view

--- application/modules/client/DefaultLayout/config/module.config.php
+++ application/modules/client/DefaultLayout/config/module.config.php
@@ -7,31 +7,46 @@ return array(
 
         'instance' => array(
             'alias' => array(
-#                'view' => 'Zend\View\PhpRenderer',
             ),
-            'Zend\View\HelperLoader' => array(
+
+            'Zend\View\Resolver\AggregateResolver' => array(
+                'injections' => array(
+                    'Zend\View\Resolver\TemplateMapResolver',
+                    'Zend\View\Resolver\TemplatePathStack',
+                ),
+            ),
+            'Zend\View\Resolver\TemplateMapResolver' => array(
                 'parameters' => array(
-                    'map' => array(
-#                       'splitbutton' => 'other\View\Helper\SplitButton',
+                    'map'  => array(
+                        'defaultlayout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+                        'defaultlayout/ate' => __DIR__ . '/../view/layout/ate.phtml',
+                        'defaultlayout/different' => __DIR__ . '/../view/layout/different.phtml',
+                        'defaultlayout/footer_one' => __DIR__ . '/../view/layout/footer_one.phtml',
+                        'defaultlayout/another' => __DIR__ . '/../view/layouts/another.phtml',
                     ),
                 ),
             ),
-            'Zend\View\HelperBroker' => array(
+            'Zend\View\Resolver\TemplatePathStack' => array(
                 'parameters' => array(
-                    'loader' => 'Zend\View\HelperLoader',
+                    'paths'  => array(
+                        'defaultlayout' => __DIR__ . '/../view',
+                    ),
                 ),
             ),
             'Zend\View\Renderer\PhpRenderer' => array(
                 'parameters' => array(
                     'resolver' => 'Zend\View\Resolver\AggregateResolver',
-                    'options' => array(
-                        'script_paths' => array(
-                            'stuql' => __DIR__ . '/../views',
-                        ),
-                    ),
-                    'broker' => 'Zend\View\HelperBroker',
                 ),
             ),
+
+
+              // enabling the code below overrides the main application module default path
+             'Zend\Mvc\View\DefaultRenderingStrategy' => array(
+                 'parameters' => array(
+                     'layoutTemplate' => 'defaultlayout/layout',
+                 ),
+             ),
+
         ),
     ),
 );
